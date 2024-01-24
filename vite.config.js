c30b4c05fc488145bb25aca10c90a1b196c7f8b6
RileyAlexis <MxRileyAlexis@gmail.com>
2024-01-24 17:35:52 -0600

    changed type from module to text/javascript

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+import meyda from 'meyda';
+import wavesurfer from 'wavesurfer.js'
+
 export default {
     optimizeDeps: {
         inlude: ['./main.js', './visualize.js'],
+        plugins: [meyda, wavesurfer],
     },
 };
